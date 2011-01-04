3305668958efcd7f50907978b35361e67dd2657b
Antons Rebguns <arebgun@gmail.com>
2011-01-04 22:44:35 +0000

    Add support for ranger tilt unit and vision pan/tilt unit. Thanks to Pablo Iñigo Blasco. Remove dependency on pr2_msgs.

--- erratic_player/src/erratic_player.cpp
+++ erratic_player/src/erratic_player.cpp
@@ -1,6 +1,7 @@
 /*
  * erratic_player
  * Copyright (c) 2010, Antons Rebguns.
+ * Copyright (c) 2010, Pablo Inigo Blasco. RTC Group - Universidad de Sevilla.
  * Copyright (c) 2008, Willow Garage, Inc.
  * All rights reserved.
  *
@@ -60,10 +61,14 @@ $ erratic_player
 
 Subscribes to (name/type):
 - @b "cmd_vel"/Twist : velocity commands to differentially drive the robot.
+- @b "cmd_vision_tilt"/Float64 : angle command to the vision tilt servo actuator (rads, from -1.5 to 1.5, home 0.2).
+- @b "cmd_vision_pan"/Float64 : angle command to the vision pan servo actuator (rads, from -1.45 to 1.45, home 0).
+- @b "cmd_ranger_tilt"/Float64 : angle command to the ranger tilt servo actuator (rads, from -1.5 to 0.95, home 0).
 
 Publishes to (name / type):
 - @b "odom"/Odometry : odometry data from the robot.
 - @b "battery_state"/PowerState : battery data. Since the robot does not have any charge detector, it uses the empirical result that the robot is charging if V>12.98
+- @b "sensor_msgs/RangeArray": Sonar Data.
 
 <hr>
 
@@ -81,31 +86,34 @@ Publishes to (name / type):
 - @b rot_decel (string) : Maximum rotational deceleration, in angle/sec/sec; nonpositive.
     Zero means use the robot's default value. Default: rot_acc.
 - @b enable_ir (bool) : Whether to enable IR sensors. Default: false.
+- @b enable_sonar (bool)  : Whether to enable Sonar sensors. Default: false.
+- @b enable_vision_pan_tilt (bool)  : Whether to enable vision servo motors on erratic. Default: false.
+- @b enable_ranger_tilt (bool)  : Whether to enable ranger servo motor on erratic. Default: false.
 - @b odometry_frame_id (string) : Frame name which will be used to send stamped transforms
     through the TF mechanism. Default: odom.
 
  **/
 
 #include <assert.h>
+#include <boost/thread.hpp>
+#include <boost/lexical_cast.hpp>
 
 // For core Player stuff (message queues, config file objects, etc.)
 #include <libplayercore/playercore.h>
 // TODO: remove XDR dependency
 #include <libplayerxdr/playerxdr.h>
 
-// roscpp
 #include <ros/ros.h>
-//rosTF
 #include "tf/transform_broadcaster.h"
 #include <tf/transform_listener.h>
 
-// Messages that I need
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
-#include <pr2_msgs/PowerState.h>
 #include <std_msgs/Float64.h>
 
-#include <boost/thread.hpp>
+#include <erratic_player/BatteryState.h>
+#include <erratic_player/RangeArray.h>
+#include <erratic_player/Range.h>
 
 #define PLAYER_QUEUE_LEN 32
 
@@ -124,7 +132,11 @@ public:
     ros::Publisher odom_pub_;
     ros::Publisher battery_pub_;
     ros::Publisher ir_pub_;
+    ros::Publisher sonar_pub;
     ros::Subscriber cmd_vel_sub_;
+    ros::Subscriber cmd_ranger_tilt_sub_;
+    ros::Subscriber cmd_vision_tilt_sub_;
+    ros::Subscriber cmd_vision_pan_sub_;
     std::string tf_prefix_;
 
     ErraticNode() : watts_charging_(10), watts_unplugged_(-10), charging_threshold_(12.98)
@@ -148,6 +160,9 @@ public:
         private_nh.getParam("rot_decel", rot_decel);
 
         private_nh.param("enable_ir", enable_ir, false);
+        private_nh.param("enable_sonar", enable_sonar, false);
+        private_nh.param("enable_vision_pan_tilt", enable_vision_pan_tilt, false);
+        private_nh.param("enable_ranger_tilt", enable_ranger_tilt, false);
         private_nh.param("odometry_frame_id", odom_frame_id, string("odom"));
 
         tf_prefix_ = tf::getPrefixParam(node_);
@@ -161,8 +176,9 @@ public:
 
         //create publishers for odometry and battery information
         odom_pub_ = node_.advertise<nav_msgs::Odometry>("odom", 1);
-        battery_pub_ = node_.advertise<pr2_msgs::PowerState>("battery_state", 1);
-        if (enable_ir) { ir_pub_ = node_.advertise<std_msgs::Float64>("ir", 1); } // TODO: create custom ir message
+        battery_pub_ = node_.advertise<erratic_player::BatteryState>("battery_state", 1);
+        if (enable_ir) { ir_pub_ = node_.advertise<erratic_player::RangeArray>("ir", 1); }
+        if (enable_sonar) { sonar_pub = node_.advertise<erratic_player::RangeArray>("sonar",1); }
 
         // The Player address that will be assigned to this device.  The format
         // is interface:index.  The interface must match what the driver is
@@ -171,6 +187,9 @@ public:
         const char* player_addr_pos = "position2d:0";
         const char* player_addr_power = "power:0";
         const char* player_addr_ir = "ir:0";
+        const char* player_addr_sonar="sonar:0";
+        const char* player_addr_vision_ptz="ptz:0";
+        const char* player_addr_ranger_tilt="tilt:::ptz:1";
 
         // Create a ConfigFile object, into which we'll stuff parameters.
         // Drivers assume that this object will persist throughout execution
@@ -180,9 +199,16 @@ public:
 
         // Insert (name,value) pairs into the ConfigFile object.  These would
         // presumably come from the param server
-        this->cf->InsertFieldValue(0, "provides", player_addr_pos);
-        this->cf->InsertFieldValue(1, "provides", player_addr_power);
-        if (enable_ir) { this->cf->InsertFieldValue(2, "provides", player_addr_ir); }
+        int field_id = 0;
+
+        this->cf->InsertFieldValue(field_id++, "provides", player_addr_pos);
+        this->cf->InsertFieldValue(field_id++, "provides", player_addr_power);
+
+        // optional erratic accessories
+        if (enable_ir) { this->cf->InsertFieldValue(field_id++, "provides", player_addr_ir); }
+        if (enable_sonar) {this->cf->InsertFieldValue(field_id++,"provides", player_addr_sonar);}
+        if (enable_vision_pan_tilt) {this->cf->InsertFieldValue(field_id++,"provides", player_addr_vision_ptz);}
+        if (enable_ranger_tilt) {this->cf->InsertFieldValue(field_id++,"provides", player_addr_ranger_tilt);}
 
         this->cf->InsertFieldValue(0, "port", port.c_str());
         this->cf->InsertFieldValue(0, "max_trans_vel", max_trans_vel.c_str());
@@ -205,7 +231,11 @@ public:
         // created as part of the driver's initialization.
         assert((this->pos_device = deviceTable->GetDevice(player_addr_pos, false)));
         assert((this->power_device = deviceTable->GetDevice(player_addr_power, false)));
+
         if (enable_ir) { assert((this->ir_device = deviceTable->GetDevice(player_addr_ir, false))); }
+        if (enable_sonar) { assert((this->sonar_device = deviceTable->GetDevice(player_addr_sonar,false))); }
+        if (enable_vision_pan_tilt) { assert((this->vision_ptz_device = deviceTable->GetDevice(player_addr_vision_ptz,false))); }
+        if (enable_ranger_tilt) { assert((this->ranger_tilt_device = deviceTable->GetDevice(player_addr_ranger_tilt,false))); }
 
         // Create a message queue
         this->q = QueuePointer(false, PLAYER_QUEUE_LEN);
@@ -238,7 +268,22 @@ public:
             return(-1);
         }
 
-        cmd_vel_sub_ = node_.subscribe<geometry_msgs::Twist>("cmd_vel", 1, boost::bind(&ErraticNode::cmdvelReceived, this, _1));
+        if (enable_sonar && (this->sonar_device->Subscribe(this->q) != 0))
+        {
+            ROS_ERROR("Failed to start the sonar device");
+            return(-1);
+        }
+
+        cmd_vel_sub_ = node_.subscribe<geometry_msgs::Twist>("cmd_vel", 1, boost::bind(&ErraticNode::cmdVelReceived, this, _1));
+
+        if (enable_vision_pan_tilt)
+        {
+            cmd_vision_pan_sub_ = node_.subscribe<std_msgs::Float64>("cmd_vision_pan", 1, boost::bind(&ErraticNode::cmdVisionPanReceived,this,_1));
+            cmd_vision_tilt_sub_ = node_.subscribe<std_msgs::Float64>("cmd_vision_tilt", 1, boost::bind(&ErraticNode::cmdVisionTiltReceived,this,_1));
+        }
+
+        if (enable_ranger_tilt) { cmd_ranger_tilt_sub_=node_.subscribe<std_msgs::Float64>("cmd_ranger_tilt", 1, boost::bind(&ErraticNode::cmdRangerTiltReceived,this,_1)); }
+
         return(0);
     }
 
@@ -262,6 +307,24 @@ public:
         if (enable_ir && (this->ir_device->Unsubscribe(this->q) != 0))
         {
             ROS_WARN("Failed to stop the ir device");
+            status = -1;
+        }
+
+        if (enable_sonar && (this->sonar_device->Unsubscribe(this->q) != 0))
+        {
+            ROS_WARN("Failed to stop the sonar device");
+            status = -1;
+        }
+
+        if (enable_vision_pan_tilt && (this->vision_ptz_device->Unsubscribe(this->q) != 0))
+        {
+            ROS_WARN("Failed to stop the vision pan/tilt device");
+            status = -1;
+        }
+
+        if (enable_ranger_tilt && (this->ranger_tilt_device->Unsubscribe(this->q) != 0))
+        {
+            ROS_WARN("Failed to stop the ranger tilt device");
             status = -1;
         }
 
@@ -294,6 +357,7 @@ public:
         }
     }
 
+
     void getCenter()
     {
         Message* msg = NULL;
@@ -315,15 +379,49 @@ public:
         delete msg;
     }
 
-    void cmdvelReceived(const geometry_msgs::Twist::ConstPtr& cmd_vel)
+    void cmdRangerTiltReceived (const std_msgs::Float64::ConstPtr& tilt)
     {
-        /*
-        printf("received cmd: (%.3f,%.3f,%.3f)\n",
-        cmd_vel->linear.x,
-        cmd_vel->linear.y,
-        cmd_vel->angular.z);
-        */
+        player_ptz_cmd req;
+        memset(&req, 0, sizeof(req));
 
+        req.tilt=tilt->data;
+
+        this->ranger_tilt_device->PutMsg(this->q,
+                                         PLAYER_MSGTYPE_CMD,
+                                         PLAYER_PTZ_CMD_STATE,
+                                         (void*) &req,
+                                         sizeof(req),NULL);
+    }
+
+    void updateVisionPanTilt()
+    {
+        player_ptz_cmd req;
+        memset(&req, 0, sizeof(req));
+
+        req.pan = vision_pan_rads;
+        req.tilt = vision_tilt_rads;
+
+        this->vision_ptz_device->PutMsg(this->q,
+                                        PLAYER_MSGTYPE_CMD,
+                                        PLAYER_PTZ_CMD_STATE,
+                                        (void*) &req,
+                                        sizeof(req),NULL);
+    }
+
+    void cmdVisionTiltReceived(const std_msgs::Float64::ConstPtr& tilt)
+    {
+        vision_tilt_rads = tilt->data;
+        updateVisionPanTilt();
+    }
+
+    void cmdVisionPanReceived(const std_msgs::Float64::ConstPtr& pan)
+    {
+        vision_pan_rads = pan->data;
+        updateVisionPanTilt();
+    }
+
+    void cmdVelReceived(const geometry_msgs::Twist::ConstPtr& cmd_vel)
+    {
         player_position2d_cmd_vel_t cmd;
         memset(&cmd, 0, sizeof(cmd));
 
@@ -393,57 +491,118 @@ public:
 
             //printf("Published new odom: (%.3f,%.3f,%.3f)\n",
             //odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.th);
-            }
-            else if ((hdr->type == PLAYER_MSGTYPE_DATA) &&
-                     (hdr->subtype == PLAYER_POWER_DATA_STATE) &&
-                     (hdr->addr.interf == PLAYER_POWER_CODE))
-            {
-                player_power_data_t* pdata = (player_power_data_t*) msg->GetPayload();
-                pr2_msgs::PowerState state;
-                if (pdata->percent < 25) { ROS_WARN("Battery capacity is at %f%% (%fV), please recharge", pdata->percent, pdata->volts); }
-                //ROS_INFO("charging = %d, joules = %f, percent = %f, volts = %f, watts = %f", pdata->charging, pdata->joules, pdata->percent, pdata->volts, pdata->watts);
-                state.header.stamp = ros::Time::now();
-                state.time_remaining.fromSec((pdata->volts < 11.5) ? 0 : 3600); //need to calculate the remaing runtime based on the batteries discharge curve, for now stop when voltage is below 11.5. -Curt
-                state.power_consumption = (pdata->volts > charging_threshold_) ? watts_charging_ : watts_unplugged_; //Does not work, as they don't publish this
-                state.AC_present = (pdata->volts > charging_threshold_) ? 1 : 0; // are we charging?
-                battery_pub_.publish(state);
-            }
-            else if ((hdr->type == PLAYER_MSGTYPE_DATA) &&
-                     (hdr->subtype == PLAYER_IR_DATA_RANGES) &&
-                     (hdr->addr.interf == PLAYER_IR_CODE))
-            {
-                player_ir_data_t* pdata = (player_ir_data_t*) msg->GetPayload();
+        }
+        else if ((hdr->type == PLAYER_MSGTYPE_DATA) &&
+                    (hdr->subtype == PLAYER_POWER_DATA_STATE) &&
+                    (hdr->addr.interf == PLAYER_POWER_CODE))
+        {
+            player_power_data_t* pdata = (player_power_data_t*) msg->GetPayload();
+            erratic_player::BatteryState state;
 
-                ROS_INFO("Received IR msg type");
+            if (pdata->percent < 25) { ROS_WARN("Battery capacity is at %f%% (%fV), please recharge", pdata->percent, pdata->volts); }
 
-                for (uint32_t i = 0 ; i < pdata->ranges_count; ++i)
-                {
-                    ROS_INFO("IR(%d) = %f m", i, pdata->ranges[i]);
-                }
-            }
-            else
+            state.header.stamp = ros::Time::now();
+            state.capacity = pdata->percent;
+            state.charge = pdata->volts;
+
+            ROS_DEBUG("percent = %f, volts = %f", pdata->percent, pdata->volts);
+            //state.time_remaining.fromSec((pdata->volts < 11.5) ? 0 : 3600); //need to calculate the remaing runtime based on the batteries discharge curve, for now stop when voltage is below 11.5. -Curt
+            //state.power_consumption = (pdata->volts > charging_threshold_) ? watts_charging_ : watts_unplugged_; //Does not work, as they don't publish this
+            //state.AC_present = (pdata->volts > charging_threshold_) ? 1 : 0; // are we charging?
+
+            battery_pub_.publish(state);
+        }
+        else if ((hdr->type == PLAYER_MSGTYPE_DATA) &&
+                 (hdr->subtype == PLAYER_SONAR_DATA_RANGES) &&
+                 (hdr->addr.interf == PLAYER_SONAR_CODE))
+        {
+            //ROS_INFO("Received Sonar msg type-> %d",hdr->addr.interf);
+            player_sonar_data_t* pdata = (player_sonar_data_t*) msg->GetPayload();
+
+            erratic_player::RangeArray rangerArray;
+            rangerArray.ranges.resize(pdata->ranges_count);
+
+            for (uint32_t i = 0 ; i < pdata->ranges_count; ++i)
             {
-                ROS_WARN("Unhandled Player message %d:%d:%d:%d",
-                          hdr->type,
-                          hdr->subtype,
-                          hdr->addr.interf,
-                          hdr->addr.index);
+                //ROS_INFO("Sonar(%d) = %f m", i, pdata->ranges[i]);
+                rangerArray.ranges[i].header.stamp = ros::Time::now();
+                rangerArray.ranges[i].header.frame_id = string("erratic_sonar_");
+                rangerArray.ranges[i].header.frame_id += boost::lexical_cast<std::string>(i);
+                rangerArray.ranges[i].range = pdata->ranges[i];
+
+                // 30 degrees following the erratic manual
+                rangerArray.ranges[i].field_of_view = 0.5236085;
+                rangerArray.ranges[i].max_range = 5;
+                rangerArray.ranges[i].min_range = 0;
+                rangerArray.ranges[i].radiation_type = erratic_player::Range::ULTRASOUND;
             }
+
+            sonar_pub.publish(rangerArray);
+        }
+        else if ((hdr->type == PLAYER_MSGTYPE_DATA) &&
+                 (hdr->subtype == PLAYER_IR_DATA_RANGES) &&
+                 (hdr->addr.interf == PLAYER_IR_CODE))
+        {
+            player_ir_data_t* pdata = (player_ir_data_t*) msg->GetPayload();
+
+            ros::Time timestamp = ros::Time::now();
+            erratic_player::RangeArray rangerArray;
+            rangerArray.ranges.resize(pdata->ranges_count);
+
+            ROS_DEBUG("Received IR msg type, ranges_count -> %d, voltages_count: %d", pdata->ranges_count, pdata->voltages_count);
+            // TODO: figure out why this segfaults
+
+            for (uint32_t i = 0 ; i < pdata->ranges_count; ++i)
+            {
+                rangerArray.ranges[i].header.stamp = timestamp;
+                rangerArray.ranges[i].header.frame_id = string("erratic_ir_");
+                rangerArray.ranges[i].header.frame_id += boost::lexical_cast<std::string>(i);
+                rangerArray.ranges[i].range = pdata->ranges[i];
+                rangerArray.ranges[i].radiation_type = erratic_player::Range::INFRARED;
+            }
+
+            ir_pub_.publish(rangerArray);
+        }
+        else
+        {
+            ROS_WARN("Unhandled Player message %d:%d:%d:%d",
+                     hdr->type,
+                     hdr->subtype,
+                     hdr->addr.interf,
+                     hdr->addr.index);
+        }
 
         // We're done with the message now
         delete msg;
     }
 
 private:
-    double center_x_, center_y_, center_yaw_;
-    double watts_charging_, watts_unplugged_, charging_threshold_;
+    double center_x_;
+    double center_y_;
+    double center_yaw_;
+
+    double watts_charging_;
+    double watts_unplugged_;
+    double charging_threshold_;
+
+    float vision_pan_rads;
+    float vision_tilt_rads;
+
     Driver* driver;
     Device* pos_device;
     Device* power_device;
+    Device* sonar_device;
     Device* ir_device;
+    Device* ranger_tilt_device;
+    Device* vision_ptz_device;
+
     ConfigFile* cf;
     std::string odom_frame_id;
+
     bool enable_ir;
+    bool enable_sonar;
+    bool enable_ranger_tilt;
+    bool enable_vision_pan_tilt;
 };
 
 void spinThread()
@@ -490,4 +649,3 @@ int main(int argc, char** argv)
     // To quote Morgan, Hooray!
     return(0);
 }
-
